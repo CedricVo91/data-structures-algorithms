93b20c6d4b217d61ceee8fa7cee491443a5c4f81
CedricVo91 <cedricvogt91@gmail.com>
2024-03-18 15:36:02 +0100

    adjusted delete function for BST

--- AlgoMonster/dfs/bst_delete.py
+++ AlgoMonster/dfs/bst_delete.py
@@ -9,48 +9,42 @@ class Node:
 # The successor of a node in a BST is the node with the smallest value that is greater than the value of the node to be deleted
 def bst_delete(tree: Node, val: int) -> Node:
 
-    parent_node = None
-    node_to_be_deleted = None
-    parent_node_tobedeleted = None  # external variable to keep track of the parent node
-    successor = None
-    parent_successor = None
-
-    def find(tree, val):
+    #helper function to recursively find the node if it exists
+    def find_and_modify(tree, val):
 
         if tree is None:
-            return -1  # placeholder for False: no Node with such a val was found
+            return tree  # by this we ensure that we do not have a node with the tb deleted node, we return our original tree
 
         if tree.val == val:
-            node_to_be_deleted = tree
-
-            # get successor if there
+            
+            if tree.left: #there is a left child, so in any case we a
+            
             if tree.right:
                 successor = tree.right
-            while successor.left is not None:
-                successor = successor.left
-            return node_to_be_deleted, successor
+                while successor.left is not None:
+                    successor = successor.left
+                temp = tree.right
+                tree = successor
+                tree.left = temp
+                tree.right = temp.right
 
-        # if we havent found our node to be deleted, we have to go down left or right, and save our current parent node
-        parent_node = tree
+            if     
+
+            return tree
 
         if tree.val > val:
-            return find(tree.left, val)
-
+            return find_and_modify(
+                tree.left, val
+            )  
+        
         else:
-            return find(tree.right, val)
+            return find_and_modify(tree.right, val)
 
-    if find(tree) == -1:
-        return tree  # we just give back the tree, no need to delete anything as the value is not there
+    #we have the current tree node, in which we actually run the recursive helper function 
+    if tree.val > val:
+        tree.left = find_and_modify(tree.left, val)  
 
-    node_to_be_deleted, parent_successor, successor = find(tree)
-    ##reassign nodes
-    # after that our parent node is the one before the node to be deleted
-    if parent_node.val > val:
-        parent_node.left = successor
-        successor.right = node_to_be_deleted.right
-        successor.left = node_to_be_deleted.left
+    elif tree.val < val:
+        tree.right = find_and_modify(tree.right)     
 
-    else:
-        parent_node.right = successor
-        successor.right = node_to_be_deleted.right
-        successor.left = node_to_be_deleted.left
+    return tree  # we just give back the tree, no need to delete anything as the value is not there
