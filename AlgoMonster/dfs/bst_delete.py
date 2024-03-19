b767a563b42305d9a7247fce89081d9795c4a422
CedricVo91 <cedricvogt91@gmail.com>
2024-03-19 14:27:24 +0100

    Final BST Deletion Function added

--- AlgoMonster/dfs/bst_delete.py
+++ AlgoMonster/dfs/bst_delete.py
@@ -9,42 +9,79 @@ class Node:
 # The successor of a node in a BST is the node with the smallest value that is greater than the value of the node to be deleted
 def bst_delete(tree: Node, val: int) -> Node:
 
-    #helper function to recursively find the node if it exists
+    # helper function to recursively find the node if it exists
     def find_and_modify(tree, val):
 
         if tree is None:
             return tree  # by this we ensure that we do not have a node with the tb deleted node, we return our original tree
 
         if tree.val == val:
-            
-            if tree.left: #there is a left child, so in any case we a
-            
-            if tree.right:
-                successor = tree.right
-                while successor.left is not None:
-                    successor = successor.left
-                temp = tree.right
-                tree = successor
-                tree.left = temp
-                tree.right = temp.right
 
-            if     
+            if (
+                tree.left is None
+            ):  # there is a no left child, we only have at best a right child
+                return tree.right
+
+            elif (
+                tree.right is None
+            ):  # there is a no right child, we only have at best a left child
+                return tree.left
+
+            # two children. no else is needed as we have two return statements in the if/elif clause
+            successor = tree.right
+            while successor.left is not None:
+                successor = successor.left
+
+            # the trickiest part: instead of switching nodes, we just amend the values and keep the left and right child
+            tree.val = successor.val
+            # left child of tree is still linked correctly
+            # adjust right child of tree by running the find and modify (aka delete function again) with value of successor
+            tree.right = find_and_modify(tree.right, successor.val)
 
             return tree
 
-        if tree.val > val:
-            return find_and_modify(
-                tree.left, val
-            )  
-        
+        elif tree.val > val:
+            return find_and_modify(tree.left, val)
+
         else:
             return find_and_modify(tree.right, val)
 
-    #we have the current tree node, in which we actually run the recursive helper function 
-    if tree.val > val:
-        tree.left = find_and_modify(tree.left, val)  
+    return find_and_modify(tree, val)
 
-    elif tree.val < val:
-        tree.right = find_and_modify(tree.right)     
 
-    return tree  # we just give back the tree, no need to delete anything as the value is not there
+# helper function to recursively find the node if it exists
+def bst_delete(tree: Node, val: int) -> Node:
+
+    if tree is None:
+        return tree  # by this we ensure that we do not have a node with the tb deleted node, we return our original tree
+
+    if tree.val == val:
+
+        if (
+            tree.left is None
+        ):  # there is a no left child, we only have at best a right child
+            return tree.right
+
+        elif (
+            tree.right is None
+        ):  # there is a no right child, we only have at best a left child
+            return tree.left
+
+        # two children. no else is needed as we have two return statements in the if/elif clause
+        successor = tree.right
+        while successor.left is not None:
+            successor = successor.left
+
+        # the trickiest part: instead of switching nodes, we just amend the values and keep the left and right child
+        tree.val = successor.val
+        # left child of tree is still linked correctly
+        # adjust right child of tree by running the find and modify (aka delete function again) with value of successor
+        tree.right = bst_delete(tree.right, successor.val)
+
+        return tree
+
+    elif tree.val > val:
+        return bst_delete(tree.left, val)
+
+    else:
+        return bst_delete(tree.right, val)
